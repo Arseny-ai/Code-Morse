80e8c3be0ea172885e1fcf6689902b2f0a08510d
Arseny <lufi.2023@gmail.com>
2022-12-10 13:57:40 +0300

    update

---
+++ index.js
@@ -0,0 +1,73 @@
+const morse = {
+  A: "•-",
+  B: "-•••",
+  C: "-•-•",
+  D: "-••",
+  E: "•",
+  F: "••-•",
+  G: "--•",
+  H: "••••",
+  I: "••",
+  J: "•---",
+  K: "-•-",
+  L: "•-••",
+  M: "--",
+  N: "-•",
+  O: "---",
+  P: "•--•",
+  Q: "--•-",
+  R: "•-•",
+  S: "•••",
+  T: "-",
+  U: "••-",
+  V: "•••-",
+  W: "•--",
+  X: "-••-",
+  Y: "-•--",
+  Z: "--••",
+  " ": "•••••••",
+};
+
+const shortNode = document.querySelector(`#short`);
+const longNode = document.querySelector(`#long`);
+const enterNode = document.querySelector(`#enter`);
+const backspaceNode = document.querySelector(`#backspace`);
+const clearNode = document.querySelector(`#clear`);
+
+let content = document.querySelector(`#main-content`);
+
+shortNode.addEventListener(`click`, function () {
+  content.innerHTML += `•`;
+});
+longNode.addEventListener(`click`, function () {
+  content.innerHTML += `-`;
+});
+enterNode.addEventListener(`click`, function () {
+  content.innerHTML += ` `;
+});
+backspaceNode.addEventListener(`click`, function () {
+  content.innerHTML = content.innerHTML.slice(0, content.innerHTML.length - 1);
+});
+clearNode.addEventListener(`click`, function () {
+  content.innerHTML = ``;
+});
+
+setInterval(checkChanges);
+before = content.innerHTML;
+
+function checkChanges() {
+  if (content.innerHTML != before) {
+    for (const letter in morse) {
+      const result = content.innerHTML.replace(`${morse[letter]} `, letter);
+      if (content.innerHTML.includes(`${morse[letter]} `)) {
+        if (!(result.includes(`-`) || result.includes(`•`))) {
+          content.innerHTML = content.innerHTML.replace(
+            `${morse[letter]} `,
+            letter
+          );
+        }
+      }
+    }
+  }
+  before = content.innerHTML;
+}
